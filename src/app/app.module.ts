6b2e2a95e73fe7a547a4fa1dc0b3b76fbe2a5b5e
hamzahouri <hamzahouri@gmail.com>
2022-10-29 13:52:46 +0100

    adding the repository component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { PanelComponent } from './panel/panel.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonalInfoComponent
+    PersonalInfoComponent,
+    PanelComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
